9ff2f34be20f017e294881285e08179ba96a9d91
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2026-03-30 23:18:48 +0100

    feat: mainpage

--- FitManager/Forms/MainForm.cs
+++ FitManager/Forms/MainForm.cs
@@ -26,5 +26,32 @@ namespace FitManager.Forms
                 lblUsuarioLogado.Text = "Bem-vindo, " + Sessao.UsuarioLogado.Usuario;
             }
         }
+
+        private void btnSocios_Click(object sender, EventArgs e)
+        {
+            SocioForm soc = new SocioForm();
+            soc.StartPosition = FormStartPosition.CenterScreen; // abrir a janela no centro da tela
+            this.Hide();
+            soc.ShowDialog();
+            this.Show();
+        }
+
+        private void btnPlanos_Click(object sender, EventArgs e)
+        {
+            PlanoAdd plan = new PlanoAdd();
+            plan.StartPosition = FormStartPosition.CenterScreen; // abrir a janela no centro da tela
+            this.Hide();
+            plan.ShowDialog();
+            this.Show();
+        }
+
+        private void btnCheckIn_Click(object sender, EventArgs e)
+        {
+            CheckInForm check = new CheckInForm();
+            check.StartPosition = FormStartPosition.CenterScreen; // abrir a janela no centro da tela
+            this.Hide();
+            check.ShowDialog();
+            this.Show();
+        }
     }
 }
